5153614acefb954fb00d17af2ed8073522ec42c5
Mdzh <melmikelito@gmail.com>
2023-08-03 19:55:26 +0300

    move structures

--- marketplace/Marketplace/Market/models.py
+++ marketplace/Marketplace/Market/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.username} - {self.role}'
