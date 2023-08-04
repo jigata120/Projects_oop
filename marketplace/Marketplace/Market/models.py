33498a34b15ac5a29766eacc0317c075659c07c5
Mihael Jigov <107353857+jigata120@users.noreply.github.com>
2023-08-04 18:10:36 +0300

    Update models.py
    DB's:
    User,Product,Cart

--- marketplace/Marketplace/Market/models.py
+++ marketplace/Marketplace/Market/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+my_choice = (
+    ('seller', 'seller'),
+    ('buyer', 'buyer'),
+)
+
+
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    role = models.CharField(max_length=100, choices=my_choice)
+
+    def __str__(self):
+        return f'{self.username} - {self.role}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.quantity}'
+
+
+class Cart(models.Model):
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f'{self.buyer} - {self.products}'
