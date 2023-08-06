90d9c7d92ff328185bb35bf5408ac6f089a8cc47
Mihael Jigov <107353857+jigata120@users.noreply.github.com>
2023-08-06 21:59:47 +0300

    Update settings to templates

--- marketplace/Marketplace/Marketplace/settings.py
+++ marketplace/Marketplace/Marketplace/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['market'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
