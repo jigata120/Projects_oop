8145d8467e1a136dda80305ce30f08545fd9b569
Mihael Jigov <melmikelito@gmail.com>
2023-08-06 22:50:35 +0300

    new code

--- marketplace/Marketplace/Market/views.py
+++ marketplace/Marketplace/Market/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+    products = Product.objects.all()
+    cart = Cart.objects.all()
+
+
+
+
+    return render(request, 'template/market/home.html', {'products': products, 'cart': cart, 'all': all})
+
+
+def about(request):
+    return render(request, 'template/market/view.html')
