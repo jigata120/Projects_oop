a2e876d9903d9419644806e750f306a1e011cc36
Mihael Jigov <pplesho@gmail.com>
2023-08-09 18:48:25 +0300

    create new dafs

--- marketplace/Marketplace/Market/views.py
+++ marketplace/Marketplace/Market/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProductForm
 from .models import *
 from django.urls import reverse
+from django.http import HttpResponse
+
+
+def add_to_cart(request, product_id):
+    # Logic to add product to cart
+    response_data = {'message': 'Product added to cart'}
+
 
 
 def create_product(request):
@@ -18,6 +25,12 @@ def create_product(request):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'template/market/product_details.html', {'product': product})
+
+
+def delete_product(request, product_id):
+    return HttpResponse(f"Product {product_id} has been deleted")
+#def add_to_cart(request, product_id):
+#    return HttpResponse(f"Product {product_id} has been added to cart")
 
 
 def home(request):
