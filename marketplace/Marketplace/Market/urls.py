e22f81c1311dcf709f926e598a0281ad93a2007d
Mihael Jigov <pplesho@gmail.com>
2023-08-14 19:11:44 +0300

    add delete edit and fix path for path cloning

--- marketplace/Marketplace/Market/urls.py
+++ marketplace/Marketplace/Market/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('about/', views.about),
     path('create/', views.create_product, name='create_product'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('delete_product/<int:product_id>/', views.delete_product, name='delete_from_cart'),
+    path('delete_product_from_cart/<int:product_id>/', views.delete_product_from_cart, name='delete_from_cart'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-
+    path (' edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
 
 
 ]
