6746a32204b21e1b5859db6b9eda606de94d283e
Mihael Jigov <pplesho@gmail.com>
2023-08-14 19:12:44 +0300

    add an edit icon after user validation

--- marketplace/Marketplace/Market/forms.py
+++ marketplace/Marketplace/Market/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('name', 'description', 'price', 'quantity', 'seller', 'image')
+        fields = ('name', 'description', 'price', 'quantity', 'image')
