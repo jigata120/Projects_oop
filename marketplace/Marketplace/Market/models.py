361d28383b6df3c8dbb021f0e9478057086ae203
Mihael Jigov <pplesho@gmail.com>
2023-08-15 21:49:15 +0300

    new db reference for the cart owner and the user Auth

--- marketplace/Marketplace/Market/models.py
+++ marketplace/Marketplace/Market/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User as AuthUser
 my_choice = (
     ('seller', 'seller'),
     ('buyer', 'buyer'),
@@ -7,14 +7,6 @@ my_choice = (
 
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    role = models.CharField(max_length=100, choices=my_choice)
-
-    def __str__(self):
-        return f'{self.username} - {self.role}'
 
 
 class Product(models.Model):
@@ -22,7 +14,7 @@ class Product(models.Model):
     description = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
-    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    seller = models.ForeignKey(AuthUser , on_delete=models.CASCADE)
     image = models.URLField()
 
     def __str__(self):
@@ -30,7 +22,7 @@ class Product(models.Model):
 
 
 class Cart(models.Model):
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(AuthUser, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, related_name='products', blank=True)
 
     def __str__(self):
