642594e81b0c76147927d4b2b528e2bf3fa37a7a
Mihael Jigov <pplesho@gmail.com>
2023-08-15 21:49:39 +0300

    add static settings

--- marketplace/Marketplace/Marketplace/settings.py
+++ marketplace/Marketplace/Marketplace/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['market', 'Users'],
+        'DIRS': ['market', 'Users', "Marketplace"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
