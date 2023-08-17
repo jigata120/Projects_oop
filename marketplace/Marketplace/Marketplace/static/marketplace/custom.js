c376478b82c4604ac7ed7d41f5990d9f745a44a9
Mihael Jigov <pplesho@gmail.com>
2023-08-17 19:15:01 +0300

    trying another js functions

--- marketplace/Marketplace/Marketplace/static/marketplace/custom.js
+++ marketplace/Marketplace/Marketplace/static/marketplace/custom.js
@@ -1,15 +1,38 @@
 // custom.js
-let counter = 0;
 
-function script() {
-    counter++;
-    document.querySelector('h1').innerHTML = counter;
+//document.addEventListener('DOMContentLoaded', function() {     -------------    });
 
-    if (counter > 10) {
-        alert(`You have reached ${counter}`);
+
+//document.querySelectorAll('button').forEach(function(button){     ---------    });
+   //button.onclick = function(){     ---------    }
+  //    document.querySelector('h1').style.color = button.dataset.color;
+
+//document.querySelector('select').onchange = function() { ----------  });
+                  //  document.querySelector('#hello').style.color = this.value; <h1  value="blue" ></h1>
+
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('#sub').disabled = true;
+    document.querySelector('#task1').onkeyup = ()=>{
+        if (document.querySelector('#task1').value.length >0 ) {
+            document.querySelector('#sub').disabled = false;
+        }   else{
+            document.querySelector('#sub').disabled = true;
+
+        }
     }
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = script;
+    document.querySelector('#form').onsubmit = () =>{
+        const task =  document.querySelector('#task1').value;
+        console.log(task.length);
+
+        const li = document.createElement('li');
+        li.innerHTML = task;
+
+        document.querySelector('#tasks2').appendChild(li);
+        document.querySelector('#task1').value = '';
+        return false;
+
+    }
 });
