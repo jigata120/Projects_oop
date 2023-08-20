9148b1abfb05655d8ab5e7bc7b594da3f599a1c7
Mihael Jigov <pplesho@gmail.com>
2023-08-20 16:25:22 +0300

    create filter function and reformat the file

--- marketplace/Marketplace/Market/views.py
+++ marketplace/Marketplace/Market/views.py
@@ -4,6 +4,32 @@ from .models import *
 from django.urls import reverse
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def filter_products_by_price(request):
+    min_price = request.GET.get('min_price')  # Get min_price from user input
+    max_price = request.GET.get('max_price')  # Get max_price from user input
+    cart = get_object_or_404(Cart, buyer=request.user)
+    cart = cart.products.all()
+    total_price = cart_price(cart)
+    if not min_price and not max_price:
+        # If both are empty, render the home page with no filters
+        products_in_price_range = Product.objects.all()
+        return redirect('home')
+    elif not min_price or not max_price:
+        if not min_price:
+            min_price=0
+        else:
+            max_price=10000
+
+    # Filter products by price range
+    products_in_price_range = Product.objects.filter(price__gte=min_price, price__lte=max_price)
+    # Handle the case where the input is not a valid decimal
+
+    return render(request, 'template/market/home.html',
+                  {'products': products_in_price_range, 'cart': cart, 'total_price': total_price,
+                   "current_user": request.user})
 
 
 @login_required
@@ -53,12 +79,12 @@ def cart_price(cart_objects):
         final_price += product.price
     return final_price
 
+
 @login_required
 def create_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-
             product = form.save(commit=False)
             product.seller = request.user  # Set the seller to the logged-in user
             product.save()
@@ -72,6 +98,7 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'template/market/product_details.html', {'product': product})
 
+
 @login_required
 def home(request):
     products = Product.objects.all()
@@ -79,7 +106,7 @@ def home(request):
     cart = cart.products.all()
     total_price = cart_price(cart)
     return render(request, 'template/market/home.html',
-                  {'products': products, 'cart': cart, 'total_price': total_price,"current_user" : request.user})
+                  {'products': products, 'cart': cart, 'total_price': total_price, "current_user": request.user})
 
 
 def about(request):
